57b2405255f2e5ac9fa5e7fa59a6ccfece80cebc
Emanuil Ganchev <emanuil.d.ganchev@gmail.com>
2026-07-27 17:06:49 +0300

    Add Tab/S-Tab mappings for cmp, luasnip, NES, inline completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -44,6 +44,27 @@ return {
           end
         end, { "i", "s" }),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif require("sidekick").nes_jump_or_apply() then
+            return
+          else
+            vim.lsp.inline_completion.get()
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
