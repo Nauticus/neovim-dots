fe7de5d4d887561049e2cf5ca003a9cf9286f636
Emanuil Ganchev <emanuil.d.ganchev@gmail.com>
2026-07-27 16:43:30 +0300

    Remove copilot.vim plugin file

--- lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "github/copilot.vim",
-  event = "VeryLazy",
-  config = function()
-    -- Copilot.vim is a Vim plugin (no Lua setup needed)
-    -- Enable copilot in all buffers
-    vim.cmd("Copilot enable")
-  end,
-}
