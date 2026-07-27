322fe05165df631c444095dba4d7329e73514783
Emanuil Ganchev <emanuil.d.ganchev@gmail.com>
2026-07-27 17:12:39 +0300

    Add luasnip expand/jump to <C-n> and <C-p>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -31,10 +31,26 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
         { name = "lazydev", priority = 1000 },
