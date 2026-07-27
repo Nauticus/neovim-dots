4e6ba0528a78d6bd2d6a1a191248fe8ea2c229e3
Emanuil Ganchev <emanuil.d.ganchev@gmail.com>
2026-07-27 15:15:06 +0300

    fix: disable sidekick NES to eliminate insert mode typing lag

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("sidekick").setup({
-      nes = { enabled = true },
+      nes = { enabled = false },
       cli = {
         tools = {
           pi = {
